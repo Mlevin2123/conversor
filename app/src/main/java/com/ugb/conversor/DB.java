cf2950e2f9f4399c8597479c8bc2470a963bc37c
Mlevin2123 <riveramelvin628@gmail.com>
2024-03-21 21:12:50 -0600

    lista amigos AQlite

--- app/src/main/java/com/ugb/conversor/DB.java
+++ app/src/main/java/com/ugb/conversor/DB.java
@@ -1,5 +1,7 @@
 package com.ugb.conversor;
+
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -8,8 +10,8 @@ import androidx.annotation.Nullable;
 public class DB extends SQLiteOpenHelper {
     private static final String dbname="amigos";
     private static final int v=1;
-    private static final String SQLdb = "CREATE TABLE amigos (idAmigo integer primary key autoincrement, nombre text, direccion text, telefono text, " +
-            "email text, dui text)";
+    private static final String SQLdb = "CREATE TABLE amigos (idAmigo integer primary key autoincrement, nombre text, " +
+            "direccion text, telefono text, email text, dui text, foto text)";
     public DB(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
         super(context, dbname, factory, v);
     }
@@ -25,11 +27,13 @@ public class DB extends SQLiteOpenHelper {
         try {
             SQLiteDatabase db = getWritableDatabase();
             String sql = "";
-            if (accion == "nuevo") {
-                sql = "INSERT INTO amigos(nombre,direccion,telefono,email,dui) VALUES('" + datos[1] + "','" + datos[2] + "','" + datos[3] + "','" + datos[4] + "','" + datos[5] + "')";
-            } else if (accion == "modificar") {
-                sql = "UPDATE amigos SET nombre='" + datos[1] + "',direccion='" + datos[2] + "',telefono='" + datos[3] + "',email='" + datos[4] + "',dui='" + datos[5] + "' WHERE idAmigo='" + datos[0] + "'";
-            } else if (accion == "eliminar") {
+            if (accion.equals("nuevo")) {
+                sql = "INSERT INTO amigos(nombre,direccion,telefono,email,dui, foto) VALUES('" + datos[1] +
+                        "','" + datos[2] + "','" + datos[3] + "','" + datos[4] + "','" + datos[5] + "', '"+ datos[6] +"')";
+            } else if (accion.equals("modificar")) {
+                sql = "UPDATE amigos SET nombre='" + datos[1] + "',direccion='" + datos[2] + "',telefono='" +
+                        datos[3] + "',email='" + datos[4] + "',dui='" + datos[5] + "', foto='"+ datos[6] +"' WHERE idAmigo='" + datos[0] + "'";
+            } else if (accion.equals("eliminar")) {
                 sql = "DELETE FROM amigos WHERE idAmigo='" + datos[0] + "'";
             }
             db.execSQL(sql);
@@ -38,5 +42,9 @@ public class DB extends SQLiteOpenHelper {
             return e.getMessage();
         }
     }
+    public Cursor consultar_amigos(){
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM amigos ORDER BY nombre", null);
+        return cursor;
+    }
 }
-
